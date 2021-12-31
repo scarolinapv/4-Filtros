84c10475b55309b4b36969821b85a7d249df925e
Carolina Plata <sucaplatava@ittepic.edu.mx>
2021-12-30 22:56:42 -0700

    Video 3

--- eje06-1/src/app/tab1/tab1.page.ts
+++ eje06-1/src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {Student} from '../models/student'
 import { StudentService } from '../services/student.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -11,11 +12,36 @@ export class Tab1Page {
 
   students: Student[] = new Array();
 
-  constructor(private studentService: StudentService) {
+  constructor(private studentService: StudentService, private alert: AlertController) {
     this.students = this.studentService.getStudents();
+  }
+
+  operation(pos: number, ev: {detail: { side }}): void {
+
+    if(ev.detail.side == 'start'){
+      this.studentService.changeStatus(pos);
+    }else{
+      this.showAlert(pos);
+    }
   }
 
   changeStatus(pos: number): void{
     this.studentService.changeStatus(pos);
+  }
+
+  async showAlert(pos: number){
+    const al = await this.alert.create({
+      header: 'Confirmar',
+      message: '¿seguro que desea eliminar?',
+      buttons: [{
+        text: 'No'
+      },{
+        text: 'Si',
+        handler: () => {
+          this.studentService.deleteStudent(pos)
+        }
+      }]
+    });
+    await al.present();
   }
 }
